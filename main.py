2f887afd4c7f892c3f77ced258f9b98575970711
Artem Ushkov <artemushkov0912@gmail.com>
2025-07-21 11:02:20 +1000

    Changes

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+import logging
+
+logger = logging.getLogger(__name__)
+log_format: str = "%(asctime)s [%(levelname)s] %(message)s"
+logging.basicConfig(format=log_format, level=logging.INFO)
+
+logger.info("Start")
+try:
+    num: int = 1 / 0
+except ZeroDivisionError as e:
+    logger.error(e)
+logger.info("Finish")
